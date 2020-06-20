355955bc5e8a20dc067c4e0af7bbecdc89fa3f55
leriou <yange1876@hotmail.com>
2020-06-20 16:51:14 +0800

    modyfy somethings

--- main.go
+++ main.go
@@ -2,35 +2,46 @@ package main
 
 import (
 	"fmt"
-	svppm "svppm/src"
+	"math"
+	"os"
 )
 
 const (
-	WIDTH  = 256
-	HEIGHT = 256
+	MAXDEEP = 255
+	WIDTH   = 256
+	HEIGHT  = 256
 )
+
+func Svppm(filename string, w, h int, row []int) bool {
+	c := fmt.Sprintf("P3\n%d %d\n%d\n", w, h, MAXDEEP)
+	for p := 0; p < len(row); p += 3 {
+		c += fmt.Sprintf("%d %d %d\n", row[p], row[p+1], row[p+2])
+	}
+	f, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	f.WriteString(c)
+	fmt.Println("write success !!!")
+	return true
+}
 
 func main() {
 	row := make([]int, WIDTH*HEIGHT*3)
-	p := 0
 	r := 0
 	g := 0
-	b := 0
+	p := 0
 	for i := 0; i < WIDTH; i++ {
 		r += 1
 		for j := 0; j < HEIGHT; j++ {
 			g += 1
 			row[p] = r
 			row[p+1] = g
-			if r > g {
-				b = r
-			} else {
-				b = g
-			}
-			row[p+2] = b
+			row[p+2] = int(math.Min(float64(r), float64(g)))
 			p += 3
 		}
 	}
-	svppm.Svppm("test.ppm", WIDTH, HEIGHT, row)
+	Svppm("test.ppm", WIDTH, HEIGHT, row)
 	fmt.Println("well,done")
 }
