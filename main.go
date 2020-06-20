ba42a0ef3ff6a229030b4d532ec8b957e5b658da
leriou <yange1876@hotmail.com>
2020-06-20 15:47:18 +0800

    ignore ppm

--- main.go
+++ main.go
@@ -6,15 +6,23 @@ import (
 )
 
 func main() {
-
 	row := make([]int, 256*256*3)
-
 	p := 0
+	r := 0
+	g := 0
+	b := 0
 	for i := 0; i <= 255; i++ {
+		r += 1
 		for j := 0; j <= 255; j++ {
-			row[p] = j
-			row[p+1] = i
-			row[p+2] = 128
+			g += 1
+			row[p] = r
+			row[p+1] = g
+			if r > g {
+				b = r
+			} else {
+				b = g
+			}
+			row[p+2] = b
 			p += 3
 		}
 	}
